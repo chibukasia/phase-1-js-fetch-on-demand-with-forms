11d8e2518331192a4d298256edd7398cead64416
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-14 21:57:53 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const inputValue = document.getElementById('searchByID').value;
+        console.log(inputValue);
+        fetch(`http://localhost:3000/movies/${inputValue}`)
+        .then(res=> res.json())
+        .then(function(movies){
+            //console.log(movies)
+            const title = document.querySelector('#movieDetails h4');
+            const summary = document.querySelector('#movieDetails p');
+            //console.log(title)
+            title.innerText = movies.title;
+            summary.innerText = movies.summary;
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
